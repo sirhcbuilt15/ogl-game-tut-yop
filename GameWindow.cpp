68390bda8d7c39f6d7d2e2f6e2a166eda3ac5c05
Christian Salomon <nomolas.sirhc@gmail.com>
2016-05-28 10:47:55 -0500

    Init Files

---
+++ GameWindow.cpp
@@ -0,0 +1,119 @@
+/*
+ * GameWindow.cpp
+ *
+ *  Created on: May 13, 2016
+ *      Author: chris
+ */
+
+# include "GameWindow.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#include "third_party/stb_image.h"
+
+// Hacemos una estructura para manejar la posición
+typedef struct {
+	GLfloat positionCoordinates[3];
+	GLfloat textureCoordinates[2];
+} VertexData;
+
+#define Square_Size 100
+
+VertexData vertices[] = {
+	{{0.0f, 0.0f, 0.0f},				{0.0f, 1.0f}},
+	{{Square_Size, 0.0f, 0.0f},			{1.0f, 1.0f}},
+	{{Square_Size, Square_Size, 0.0f},	{1.0f, 0.0f}},
+	{{0.0f, Square_Size, 0.0f},			{0.0f, 0.0f}}
+};
+
+void GameWindow::setRunning(bool newRunning)
+{
+	_running = newRunning;
+}
+
+bool GameWindow::getRunning()
+{
+	return _running;
+}
+
+GLuint GameWindow::loadAndBufferImage(const char *filename)
+{
+	int width, height, bpp /* 8-bit component per pixel*/;
+	unsigned char *data = NULL;
+
+	GLuint textureBufferID;
+
+	data = stbi_load(filename, &width, &height, &bpp, 0);
+	if( data == NULL )
+		return 0;
+
+	glGenTextures(1, &textureBufferID);
+	glBindTexture(GL_TEXTURE_2D, textureBufferID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	stbi_image_free(data);
+
+	return textureBufferID;
+}
+
+GameWindow::GameWindow(bool running, GLFWwindow* window): _running(running), _height(800), _width(800*16/9), _vertexBufferID(0)
+{
+	_window = window;
+	glClearColor(0.5f, 0.2f, 0.45f, 0.5f);
+	glViewport(0.0f, 0.0f, _width, _height);
+
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0, _width, 0.0, _height);
+	glMatrixMode(GL_MODELVIEW);
+
+	glGenBuffers(1, &_vertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, sizeof(VertexData), (GLvoid *) offsetof(VertexData, positionCoordinates) );
+
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glTexCoordPointer(2, GL_FLOAT, sizeof(VertexData), (GLvoid *) offsetof(VertexData, textureCoordinates) );
+
+	_textureBufferID = loadAndBufferImage("nave.png");
+}
+
+void GameWindow::render()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/*part5Tutotial
+	glColor3f(0.9f, 0.9f, 0.9f);
+	glBegin(GL_QUADS);
+	glVertex2d( 100.0f, 100.0f);
+	glVertex2d( 200.0f, 100.0f);
+	glVertex2d( 200.0f, 200.0f);
+	glVertex2d( 100.0f, 200.0f);
+	glEnd();
+     	*/
+
+	//glColor3f(0.45f, 1.0f, 0.95f);
+	glDrawArrays(GL_QUADS, 0, 4);
+
+   	glfwSwapBuffers(_window);
+}
+
+void GameWindow::update()
+{
+
+}
+
+
+
+
+
---
+++ GameWindow.h
@@ -0,0 +1,44 @@
+/*
+ * GameWindow.h
+ *
+ *  Created on: May 13, 2016
+ *      Author: chris
+ */
+
+#ifndef _SimpleOpenGLGame_GameWindow_
+#define _SimpleOpenGlGame_GameWindow_
+
+/* To use glGenBuffers and others */
+#define GL_GLEXT_PROTOTYPES
+#define GLFW_INCLUDE_GLEXT
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>
+#include <vector>
+
+class GameWindow
+{
+private:
+	bool _running;
+	GLfloat _height;
+	GLfloat _width;
+	GLuint _vertexBufferID;
+	GLuint _textureBufferID;
+	GLuint loadAndBufferImage(const char *filename);
+
+	GLFWwindow* _window;
+
+public:
+	void setRunning( bool newRunning );
+	bool getRunning();
+
+	GameWindow( bool running, GLFWwindow* window );
+
+	void render();
+	void update();
+};
+
+#endif /*defined _SimpleOpenGLGame_GameWindow_ */
